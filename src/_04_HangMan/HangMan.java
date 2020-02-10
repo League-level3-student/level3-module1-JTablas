4bd51d909cf256b8ad182df7c123a177b4edad0b
JTablas <Jason.Tablas@gompersstudent.org>
2020-02-10 14:50:50 -0800

    Update HangMan.java

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,13 +1,31 @@
 package _04_HangMan;
 
-import javax.swing.JOptionPane;
+import java.util.Stack;
 
-public class HangMan{
-	
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan {
+
 	public static void main(String[] args) {
 		String wordcho = JOptionPane.showInputDialog("Choose a number between 1 and 266.");
 		int wordnum = Integer.parseInt(wordcho);
-	Utilities.readRandomLineFromFile(wordcho);
+		System.out.println(wordnum);
+		Stack<Utilities> util = new Stack<Utilities>();
+
+		Stack<String> hang = new Stack<String>();
+
+		for (int i = 0; i < wordnum; i++) {
+			hang.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+			System.out.println(hang);
+		}
+		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+
+		
+		
 	}
-	
+
 }
